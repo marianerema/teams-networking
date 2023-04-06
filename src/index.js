b3f56a2eff821e5629c27cf3d10a6c159574f0fe
marianerema <116829661+marianerema@users.noreply.github.com>
2023-04-06 20:45:08 +0300

    improve load of debounce

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { debounce } from "lodash";
+import debounce from "lodash/debounce";
 import { createTeamRequest, loadTeamRequest, updateTeamRequest, deleteTeamRequest } from "./requests";
 import { $, sleep } from "./utilities";
 
